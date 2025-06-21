5ed7a9c1167c64d506133d5b8f6a161169ce5edf
meganathan-repo <meganathann044@gmail.com>
2025-06-21 12:44:27 +0530

    Update exapmle.py

--- exapmle.py
+++ exapmle.py
@@ -1,5 +1,6 @@
-api_key = "ghp_123456789abcdefghijklmnopqrstuvwxyz123"
+api_key = "ghp_abcdefghijklmnopqrstuvwxyz1234567890ab"
 aws_key = "AKIAIOSFODNN7EXAMPLE"
+aws_secret = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 #l;l;l;l;kldsjd
 #dflkjldmzlkzdfcjsklmdl
 #dff
