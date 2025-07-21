5563a29f14d93870f79092cbe6beb31a72b060fe
meganathan-repo <meganathann044@gmail.com>
2025-07-21 12:01:16 +0530

    Update exapmle.py

--- exapmle.py
+++ exapmle.py
@@ -3,7 +3,7 @@ aws_key = "AKIAIOSFODNN7EXAMPLE"
 aws_secret = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 #l;l;l;l;kldsjd
 #dflkjldmzlkzdfcjsklmdlkmmllk
-#mdfkdgl
+#mdfkdgldfkjlkjlaj
 openai_key = "sk-live-aB1cD2eF3gHiJ4klMN5OPQrSTuVwXyZ6ABCD78"
 
 #dffdf;l
