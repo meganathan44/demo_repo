c708958b19f7678d088798261cdfd1af0aaea089
testing_project <meganathann044@gmail.com>
2025-07-28 17:08:10 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ def calculate_sum a, b
 print("Sum is" a + b)  
     result = 10 / 0     
 x = "hello" + 5       
-if x == 5            #,mdskjlafskjflmdfs;ldfiioijof,lvkljkjldf
+if x == 5            #,mdskjlafskjflmdfs;ldfiioijof,lvkljkjldfdfskds
     print(x          
 def multiply(x y)      
     return x * y      
