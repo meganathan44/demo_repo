6f2d0065ac9f5b251b4d8f5a452f408cd6317145
testing_project <meganathann044@gmail.com>
2025-07-29 12:42:55 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
 def calculate_sum a, b  
 print("Sum is" a + b)  
-    result = 10 / 0     
-x = "hello" + 5       
-if x == 5            #,mdskslks
-    print(x          
-def multiply(x y)      
-    return x * y      
-z = undefined_var    
-print("End"           
+        
