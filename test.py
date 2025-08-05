b1bec945b04ca0d50e8408e50b58dfab6e4de338
testing_project <meganathann044@gmail.com>
2025-08-05 13:43:28 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 def calculate_sum a, b  # Missing parentheses and colonn
 x = 5 +  # Incomplete expressions
-if x > 3  # Missing colon 
+if x > 3  # Missing colon find
     print("x is large"  # Missing closing parenthesis
 while True print("loop")  # Missing colon after while
 y = [1, 2, 3  # Missing closing bracket
