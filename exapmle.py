25a45df4efaf5856a7aefde1e777fd661c77aa60
testing_project <meganathann044@gmail.com>
2025-10-03 16:03:50 +0530

    Update exapmle.py

--- exapmle.py
+++ exapmle.py
@@ -5,7 +5,7 @@ aws_secret = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 #dflkjdfvddd
 #mdfkdgldfkj
 openai_key = "sk-live-aB1cD2eF3gHiJ4klMN5OPQrSTuVwXyZ6ABCD78"
-#dfjldgsij
+#dfjldgsi
 #dffdf;lm
 aws_key = "AKIAIOSFODNN7EXAMPLE"
 aws_secret = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
